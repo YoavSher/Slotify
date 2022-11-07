d1ba2a2cb0ee8a1d6d7add34459403cdfb2bb10c
Yoav <yoav.sher11@gmail.com>
2022-11-07 19:05:25 +0200

    first commit

---
+++ src/cmps/stam.tsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+import YouTube, { YouTubeProps } from 'react-youtube';
+
+export const Example = () => {
+
+    let playerRef = useRef<any>()
+    let player!: any
+
+    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+        // access to player in all event handlers via event.target
+        event.target.playVideo();
+        playerRef.current = event.target
+    }
+    const startVideo = () => {
+        playerRef.current.playVideo()
+        // console.log('playerRef.current: ',playerRef)
+    }
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+    return (
+        <>
+            <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={onPlayerReady} />
+            <button onClick={startVideo}>play</button>
+        </>
+    )
+
+
+    // _onReady(event: any) {
+    //     // access to player in all event handlers via event.target
+
+    //     event.target.pauseVideo();
+    // }
+}
+
